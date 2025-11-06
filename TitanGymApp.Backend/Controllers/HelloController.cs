55eb2e482510ffe0e404667017737a5fa923fa10
AntonioMelino <antoniomelino1997@gmail.com>
2025-11-06 19:51:39 -0300

    feat(api): add /api/hello endpoint
    feat(api): add /api/hello endpoint
    
    - Create HelloController with /api/hello route.
    - Return basic greeting message for testing.

---
+++ TitanGymApp.Backend/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TitanGymApp.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok(new { message = "Hola desde .NET!" });
+        }
+    }
+}
+
